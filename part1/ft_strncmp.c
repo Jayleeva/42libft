7292bde7ef9c89b86c2f6a3db6ad1fdfa7597d1b
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2024-10-25 14:46:49 +0200

    Update ft_strncmp.c

--- part1/ft_strncmp.c
+++ part1/ft_strncmp.c
@@ -20,8 +20,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while ((s1[i] || s2[i]) && i < n)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] != s2[i])
+			return ((char)s1[i] - (char)s2[i]);
 		i ++;
 	}
 	return (0);
