63303aae4ed0bef7c2cbbdb404674c8441aff693
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2024-10-25 15:27:01 +0200

    Update ft_memcmp.c

--- part1/ft_memcmp.c
+++ part1/ft_memcmp.c
@@ -16,16 +16,16 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t		i;
-	const char	*s1_;
-	const char	*s2_;
+	char	*s1_;
+	char	*s2_;
 
-	s1_ = s1;
-	s2_ = s2;
+	s1_ = (char *)s1;
+	s2_ = (char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if ((unsigned char)s1_[i] != (unsigned char)s2_[i])
-			return ((unsigned char)s1_[i] - (unsigned char)s2_[i]);
+		if (s1_[i] != s2_[i])
+			return (s1_[i] - s2_[i]);
 		i ++;
 	}
 	return (0);
