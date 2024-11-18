96cf9787bd1c65f6d29026ada539a93fbfe0414d
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2024-11-18 14:32:36 +0100

    Update ft_calloc.c

--- part1/ft_calloc.c
+++ part1/ft_calloc.c
@@ -16,17 +16,11 @@
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
 	char	*ptr;
-	size_t	i;
 
 	ptr = malloc(nelem * elsize);
 	if (ptr == NULL)
 		return (NULL);
-	i = 0;
-	while (i < nelem * elsize)
-	{
-		ptr[i] = 0;
-		i ++;
-	}
+	ptr = ft_memset(ptr, 0, nelem * elsize);
 	return (ptr);
 }
 
